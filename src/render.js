9dff43a639dc5fe52f6cc45c41a6c4da83300b13
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-03-05 17:08:08 +0530

    😄 Implemented Capture Process

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
 const { desktopCapturer,remote } = require('electron');
-
+const { writeFile } = require('fs');
 const { Menu } = remote;
 
 
@@ -30,6 +30,9 @@ async function getVideoSources(){
     videoOptionsMenu.popup();
 }
 
+let mediaRecorder; // capture footage
+const recordedChunks = [];
+
 // Change teh videoSource window to record
 async function selectSource(source){
     videoSelectBtn.innerHTML = source.name;
@@ -50,4 +53,38 @@ async function selectSource(source){
     // Preview the source in a video element
     videoElement.srcObject = stream;
     videoElement.play();
+
+    // Create the Media Recorder
+    const options = { mimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    // Register Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+}
+
+// Captures all recorded chunks
+function handleDataAvailable(e){
+    console.log('video data available');
+    recordedChunks.push(e.data);
+}
+
+
+
+// Saves the video file on stop
+async function handleStop(e){
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Save video',
+        defaultPath: `vid-${Data.now()}.webm`
+    });
+
+    console.log(filePath);
+
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
 }
